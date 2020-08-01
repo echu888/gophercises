191df428751915c7af66c33290b27fb914b31e55
Eric Chu <chu.eric@gmail.com>
2020-08-01 10:30:30 +0700

    Add solution for Exercise #2: urlshort

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,8 +1,17 @@
-package urlshort
+package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
+
+// RedirectHandler uses a simple HTTP redirect, along with 
+// an HTTP 301 (Permanently Moved) status
+func RedirectHandler(w http.ResponseWriter, r *http.Request, newUrl string) {
+	http.Redirect(w, r, newUrl, http.StatusMovedPermanently)
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -11,9 +20,23 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		if fullurl, ok := pathsToUrls[r.URL.Path]; ok {
+			RedirectHandler(w,r,fullurl)
+		} else {
+			fallback.ServeHTTP(w,r)
+		}
+        }
 }
+
+// Converted from YAML into golang struct, using the
+// "YAML to go" service available here: 
+// https://yaml.to-go.online/
+type YamlEntries []struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
 
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
@@ -32,6 +55,15 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+
+	shortcuts := make(map[string]string)
+
+	var yamlEntries YamlEntries
+	err := yaml.Unmarshal([]byte(yml), &yamlEntries)
+	if err == nil {
+		for _,shortcut := range yamlEntries {
+			shortcuts[shortcut.Path]=shortcut.URL
+		}
+	}
+        return MapHandler(shortcuts, fallback), err
 }
